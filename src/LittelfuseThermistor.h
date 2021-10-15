2191bfe7c1c23819e4a71d873f50b04a052ab332
cedarlakeinstruments <lyndon@cedarlakeinstruments.com>
2021-10-15 06:10:24 -0500

    Initial version

---
+++ src/LittelfuseThermistor.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Resistance-temperature curve for Littelfuse thermistor
+// 5 degree C spacing
+const double ResistanceToCelsiusTable[] =
+{
+	// Start at -30C
+
+	// End at 110C
+};
+
+const double ThermistorLowerLimit = -30.0;
+const double ThermistorDegreeIncrement = 5.0;
