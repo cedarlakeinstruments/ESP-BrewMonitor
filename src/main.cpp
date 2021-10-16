42d8885a8a405517e12b4d52c866eb0472f47f6b
cedarlakeinstruments <lyndon@cedarlakeinstruments.com>
2021-10-16 12:02:24 -0500

    Convert to degrees F.

--- src/main.cpp
+++ src/main.cpp
@@ -15,16 +15,20 @@
 
 void handleRoot();
 void handleNotFound();
-char* buildPage(int p1, int p2);
+char* buildPage(int p1, int p2, float f);
 float Map(double input, float inMin, float inMax, float outMin, float outMax);
+float readTemp(void);
+float getThermistorReading(float);
+float cToF(float c);
 
 const char* ssid = STASSID;
 const char* password = STAPSK;
 char pageBuffer[1000];
+float _temperature = readTemp();
 
 ESP8266WebServer server(80);
 const int led = LED_BUILTIN;
-char* buildPage(int p1, int p2)
+char* buildPage(int p1, int p2, float temp)
 {
     const char* page =
         "<head>\
@@ -38,6 +42,7 @@ char* buildPage(int p1, int p2)
              <div class='status'>Time now %d</div>\
              <div class='status'>D1 %d</div>\
              <div class='status'>D2 %d</div>\
+             <div class='status'>%5.1fF</div>\
              <div class='control'>\
                 <form action='/LED_ON' method='GET'>\
                     <input type='submit' value='LED ON'/>\
@@ -48,134 +53,159 @@ char* buildPage(int p1, int p2)
             </div>\
          </body>";
 
-    sprintf(pageBuffer, page, millis(),p1, p2);
+    sprintf(pageBuffer, page, millis(),p1, p2, temp);
     return pageBuffer;
 }
 
-void handleRoot() 
+void handleRoot()
 {
-  server.send(200, "text/html", buildPage(100,150));
+    server.send(200, "text/html", buildPage(100, 150, _temperature));
 }
 
-void handleNotFound() 
+void handleNotFound()
 {
-  String message = "File Not Found\n\n";
-  message += "URI: ";
-  message += server.uri();
-  message += "\nMethod: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "\nArguments: ";
-  message += server.args();
-  message += "\n";
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-  }
-  server.send(404, "text/plain", message);
+    String message = "File Not Found\n\n";
+    message += "URI: ";
+    message += server.uri();
+    message += "\nMethod: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "\nArguments: ";
+    message += server.args();
+    message += "\n";
+    for (uint8_t i = 0; i < server.args(); i++)
+    {
+        message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+    }
+    server.send(404, "text/plain", message);
 }
 
-void setup(void) 
+void setup(void)
 {
-  pinMode(led, OUTPUT);
-  digitalWrite(led, 0);
+    pinMode(led, OUTPUT);
+    digitalWrite(led, 0);
 
-  Serial.begin(115200);
-//  WiFi.mode(WIFI_STA);
-  WiFi.mode(WIFI_AP);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+    Serial.begin(115200);
+    //  WiFi.mode(WIFI_STA);
+    WiFi.mode(WIFI_AP);
+    WiFi.begin(ssid, password);
+    Serial.println("");
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
 
-  if (MDNS.begin("esp8266"))
-  {
-      MDNS.addService("http", "tcp", 80);
-      // Start the mDNS responder for esp8266.local
-      Serial.println("mDNS responder started");
-  }
-  else
-  {
-      Serial.println("Error setting up MDNS responder!");
-  }
+    if (MDNS.begin("esp8266"))
+    {
+        MDNS.addService("http", "tcp", 80);
+        // Start the mDNS responder for esp8266.local
+        Serial.println("mDNS responder started");
+    }
+    else
+    {
+        Serial.println("Error setting up MDNS responder!");
+    }
 
-  server.on("/", handleRoot);
+    server.on("/", handleRoot);
 
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-  });
+    server.on("/inline", []()
+              { server.send(200, "text/plain", "this works as well"); });
 
-  server.on("/gif", []() {
-    static const uint8_t gif[] PROGMEM = {
-      0x47, 0x49, 0x46, 0x38, 0x37, 0x61, 0x10, 0x00, 0x10, 0x00, 0x80, 0x01,
-      0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0x2c, 0x00, 0x00, 0x00, 0x00,
-      0x10, 0x00, 0x10, 0x00, 0x00, 0x02, 0x19, 0x8c, 0x8f, 0xa9, 0xcb, 0x9d,
-      0x00, 0x5f, 0x74, 0xb4, 0x56, 0xb0, 0xb0, 0xd2, 0xf2, 0x35, 0x1e, 0x4c,
-      0x0c, 0x24, 0x5a, 0xe6, 0x89, 0xa6, 0x4d, 0x01, 0x00, 0x3b
-    };
-    char gif_colored[sizeof(gif)];
-    memcpy_P(gif_colored, gif, sizeof(gif));
-    // Set the background to a random set of colors
-    gif_colored[16] = millis() % 256;
-    gif_colored[17] = millis() % 256;
-    gif_colored[18] = millis() % 256;
-    server.send(200, "image/gif", gif_colored, sizeof(gif_colored));
-  });
+    server.on("/gif", []()
+              {
+                  static const uint8_t gif[] PROGMEM = {
+                      0x47, 0x49, 0x46, 0x38, 0x37, 0x61, 0x10, 0x00, 0x10, 0x00, 0x80, 0x01,
+                      0x00, 0x00, 0x00, 0x00, 0xff, 0xff, 0xff, 0x2c, 0x00, 0x00, 0x00, 0x00,
+                      0x10, 0x00, 0x10, 0x00, 0x00, 0x02, 0x19, 0x8c, 0x8f, 0xa9, 0xcb, 0x9d,
+                      0x00, 0x5f, 0x74, 0xb4, 0x56, 0xb0, 0xb0, 0xd2, 0xf2, 0x35, 0x1e, 0x4c,
+                      0x0c, 0x24, 0x5a, 0xe6, 0x89, 0xa6, 0x4d, 0x01, 0x00, 0x3b};
+                  char gif_colored[sizeof(gif)];
+                  memcpy_P(gif_colored, gif, sizeof(gif));
+                  // Set the background to a random set of colors
+                  gif_colored[16] = millis() % 256;
+                  gif_colored[17] = millis() % 256;
+                  gif_colored[18] = millis() % 256;
+                  server.send(200, "image/gif", gif_colored, sizeof(gif_colored));
+              });
 
-  
-  server.onNotFound(handleNotFound);
+    server.onNotFound(handleNotFound);
 
-  server.begin();
-  Serial.println("HTTP server started");
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
-void loop(void) 
+void loop(void)
 {
-  server.handleClient();
-  MDNS.update();
+    static unsigned long lastTime = 0;
+    if (millis() - lastTime > 5000)
+    {
+        lastTime = millis();
+        _temperature = readTemp();
+    }
+
+    server.handleClient();
+    MDNS.update();
 }
 
-float GetThermistorReading(float volts)
+///
+/// Returns temp in F
+float readTemp(void)
 {
-	// Convert voltage to resistance
-	const double Rs = 12000.0;
-	double ohms = (Rs * volts) / (3.0 - volts);
-	
-  // Check for out of range
-	if (ohms > THERMISTOR_END)
-	{
-		return ResistanceToCelsiusTable[0];
-	}
-	else if (ohms < THERMISTOR_START)
-	{
-		return ResistanceToCelsiusTable[380];
-  }
+    float volts = analogRead(A0) * 3.0 / 1024.0;
+    float temp = getThermistorReading(volts);
+    return temp;
+}
 
-	// Find position in table
-	const int sizeofTable = sizeof(ResistanceToCelsiusTable) / sizeof(ResistanceToCelsiusTable[0]);
-	
-  int i = 1;
-	for (i = 1; i < sizeofTable; i++)
-	{
-		if (ResistanceToCelsiusTable[i] <= ohms)
-		{
-			break;
-		}
-	}
+float getThermistorReading(float volts)
+{
+    // Convert voltage to resistance
+    const double Rs = 12000.0;
+    double ohms = (Rs * volts) / (3.0 - volts);
+    float temp = 0;
 
-  	// Calculate temperature based on position on curve
-    return THERMISTOR_START+i-1;
+    // Check for out of range
+    const int sizeofTable = sizeof(ResistanceToCelsiusTable) / sizeof(ResistanceToCelsiusTable[0]);
+    if (ohms > ResistanceToCelsiusTable[0])
+    {
+        temp = THERMISTOR_START;
+    }
+    else if (ohms < ResistanceToCelsiusTable[sizeofTable - 1])
+    {
+        temp = THERMISTOR_END;
+    }
+    else
+    {
+        // Find position in table
+        int i = 1;
+        for (i = 1; i < sizeofTable; i++)
+        {
+            if (ResistanceToCelsiusTable[i] <= ohms)
+            {
+                break;
+            }
+        }
+        temp = THERMISTOR_START + i - 1;
+    }
+
+    temp = cToF(temp);
+    Serial.printf("v: %8.2f, o:%8.2f, t:%8.2f\n", volts, ohms, temp);
+    return temp;
+}
+
+float cToF(float c)
+{
+    float f= c * 9.0/5.0 + 32.0;
+    return f;
 }
 
 float Map(double input, float inMin, float inMax, float outMin, float outMax)
 {
-	return (input - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+    return (input - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
-
